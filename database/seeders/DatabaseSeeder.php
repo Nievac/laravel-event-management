5982a05f8d607c9fcc2ec0b28cdf0faaaceceb3f
Christian Nieva <rhixen14@gmail.com>
2023-04-28 16:25:32 +0800

    * Added Laravel Permission package. * Modified the login throttle * Added Create User and assigned roles in DatabaseSeeder * Fixed the welcome.blade.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +17,22 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $admin_user = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@mailinator.com',
+        ]);
+        $coordinator_user = User::factory()->create([
+            'name' => 'Coordinator',
+            'email' => 'coordinator@mailinator.com',
+        ]);
+        $participant_user = User::factory()->create([
+            'name' => 'Participant 1',
+            'email' => 'participant@mailinator.com',
+        ]);
+
+        $role1 = Role::create(['name' => 'admin']);
+        $role2 = Role::create(['name' => 'coordinator']);
+        $admin_user->assignRole($role1);
+        $coordinator_user->assignRole($role2);
     }
 }
